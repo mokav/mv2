c164b8c7f74f3646172217d8013e3487ea0c1726
Moka Vuckovic <mvuckovic@gmail.com>
2023-03-27 15:55:54 +0200

    kraj prvog dana

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+let m = 17;
+let rata = 1480;
+let dug;
+let poruka = "Duzan sam "
 
-
+{
+dug = m * rata;
+poruka += dug
+}
 app.get('/', (req, res) => {
-  res.send('Zdravo svima')
+  res.send(poruka )
+
 })
 
 app.listen(port, () => {
