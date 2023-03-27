d0f02a2a56a36d2b5e69a73e53f65bb458c1fa44
Moka Vuckovic <mvuckovic@gmail.com>
2023-03-27 12:47:58 +0200

    promijenjeno na ubuntu

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Zdravo svima')
 })
 
 app.listen(port, () => {
